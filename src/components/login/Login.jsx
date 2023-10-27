4cb199a367188bb8038f7b1d565c8e7319c009e6
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-10-27 23:52:05 +0330

    fix onfocus input login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,22 +1,26 @@
 import styles from "./Login.module.scss";
 import usericon from "../../assets/loginpage/user-outlined.svg";
 import passicon from "../../assets/loginpage/lock-password.svg";
+import { useRef } from "react";
 function Login() {
-  const handleLabel = () => {
-    console.log(1);
+  const refPhoneSpan = useRef(null);
+  const refPassSpan = useRef(null);
+  const handleFocus = () => {
+    const phone = refPhoneSpan.current;
+    const pass = refPassSpan.current;
   };
   return (
     <div className={styles.container}>
       <p className={styles.title_login}>ورود به حساب</p>
       <div className={styles.loginBox}>
         <div className={styles.phone_login}>
-          <span>شماره تلفن</span>
-          <input onFocus={handleLabel} type="text" />
+          <span ref={refPhoneSpan}>شماره تلفن</span>
+          <input onFocus={handleFocus} required="" type="text" name="text" autoComplete="off" className={styles.input} />
           <img src={usericon} alt="نام کاربری" />
         </div>
         <div className={styles.password_login}>
-          <span>رمز عبور</span>
-          <input type="text" />
+          <span ref={refPassSpan}>رمز عبور</span>
+          <input type="password" autoComplete="off" />
           <img src={passicon} alt="" />
         </div>
         <input className={styles.submitLogin} type="submit" value="ورود" />
