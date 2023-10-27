e56a9550988b28ded472e8738ee18abe3d425427
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-10-28 02:10:06 +0330

    handle change login and signup components

--- src/components/login/HandleLogin.jsx
+++ src/components/login/HandleLogin.jsx
@@ -1,24 +1,47 @@
 import styles from "./HandleLogin.module.scss";
 import arrowicon from "../../assets/loginpage/arrow.svg";
 import Login from "./Login";
+import { useRef, useState } from "react";
 
 function HandleLogin() {
+  const refboxSignin = useRef(null);
+  const refboxSignup = useRef(null);
+
+  const [statusSign, setStatusSign] = useState(false);
+  const changeSign = (element) => {
+    const target = element.target.dataset.set;
+    if (target === "in") {
+      refboxSignin.current.style.width = "60%";
+      refboxSignup.current.style.width = "40%";
+    } else {
+      refboxSignup.current.style.width = "60%";
+      refboxSignin.current.style.width = "40%";
+    }
+    setStatusSign(!statusSign);
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.boxSignup}>
+      <div data-set="up" ref={refboxSignup} className={styles.boxSignup}>
         <div className={styles.filterEffect}></div>
-        <div className={styles.tableDetail}>
-          <p>ثبت نام</p>
-          <img src={arrowicon} alt="arrow icon" />
-        </div>
+        {statusSign ? (
+          <Login />
+        ) : (
+          <div onClick={changeSign} className={styles.tableDetail}>
+            <p>ثبت نام</p>
+            <img src={arrowicon} alt="arrow icon" />
+          </div>
+        )}
       </div>
-      <div className={styles.boxLogin}>
+      <div data-set="in" ref={refboxSignin} className={styles.boxLogin}>
         <div className={styles.filterEffect}></div>
-        <Login />
-        {/* <div className={styles.tableDetail}>
-          <p>ورود به حساب</p>
-          <img src={arrowicon} alt="arrow icon" />
-        </div> */}
+        {!statusSign ? (
+          <Login />
+        ) : (
+          <div onClick={changeSign} className={styles.tableDetail}>
+            <p>ورود به حساب</p>
+            <img src={arrowicon} alt="arrow icon" />
+          </div>
+        )}
       </div>
     </div>
   );
