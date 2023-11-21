ad1ec8e683ba03de4db8e0db0b8841d8547fa842
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-11-21 18:50:33 +0330

    add swiper slider in section2 in home page

--- src/components/landing/HomePage.jsx
+++ src/components/landing/HomePage.jsx
@@ -1,9 +1,11 @@
 import HeaderHomePage from "./HeaderHomePage";
 import styles from "./HomePage.module.scss";
+import SectionTwoHomePage from "./SectionTwoHomePage";
 function HomePage() {
   return (
     <div className={styles.homeContainer}>
       <HeaderHomePage />
+      <SectionTwoHomePage />
     </div>
   );
 }
--- src/components/landing/SectionTwoHomePage.jsx
+++ src/components/landing/SectionTwoHomePage.jsx
@@ -1,8 +1,53 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import im1 from "../../assets/landing/sec1.jpg";
+import im2 from "../../assets/landing/sec2.jpg";
+import im3 from "../../assets/landing/sec3.jpg";
+import im4 from "../../assets/landing/sec4.jpg";
+
 import styles from "./SectionTwoHomePage.module.scss";
 function SectionTwoHomePage() {
   return (
     <div className={styles.container}>
-      <p>ok</p>
+      <div className={styles.headerContainer}>
+        <h2>فیلم هفته بعدی را شما انتخاب کنید</h2>
+      </div>
+      <div className={styles.imageContainer}>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide style={{ width: "250px" }}>
+            <img src={im1} style={{ width: "250px" }} />
+          </SwiperSlide>
+          <SwiperSlide style={{ width: "250px" }}>
+            <img src={im2} style={{ width: "250px" }} />
+          </SwiperSlide>
+          <SwiperSlide style={{ width: "250px" }}>
+            <img src={im3} style={{ width: "250px" }} />
+          </SwiperSlide>
+          <SwiperSlide style={{ width: "250px" }}>
+            <img src={im4} style={{ width: "250px" }} />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
