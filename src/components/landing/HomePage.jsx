e2eed209ae1a4f117d0f3b5633d9d5e585a1e2cb
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2023-12-22 21:23:11 +0330

    add document.title for home page

--- src/components/landing/HomePage.jsx
+++ src/components/landing/HomePage.jsx
@@ -2,8 +2,12 @@ import HeaderHomePage from "./HeaderHomePage";
 import HandleSearch from "./SearchMovie/HandleSearch";
 import styles from "./HomePage.module.scss";
 import SectionTwoHomePage from "./SectionTwoHomePage";
+import { useEffect } from "react";
 
 function HomePage() {
+  useEffect(() => {
+    document.title = "صفحه اصلی";
+  }, []);
   return (
     <div className={styles.homeContainer}>
       <HeaderHomePage />
