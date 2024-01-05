e2caa6fb7a8951d8c54d664e7041c9b1fa63e4cd
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2024-01-05 18:14:33 +0330

    add notFoundPage

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,7 +1,11 @@
+import styles from "./NotFoundPage.module.scss";
 function NotFoundPage() {
   return (
-    <div>
-      <p>not found page !</p>
+    <div className={styles.mainContainer}>
+      <div className={styles.error404}>
+        <h1>404</h1>
+        <p>صفحه مورد نظر وجود ندارد</p>
+      </div>
     </div>
   );
 }
