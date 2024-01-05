59f0ec7341c32b02c56795f8957fe25ea278baa5
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2024-01-05 18:36:33 +0330

    finish notfoundpage

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,10 +1,22 @@
 import styles from "./NotFoundPage.module.scss";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 function NotFoundPage() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.error404}>
         <h1>404</h1>
         <p>صفحه مورد نظر وجود ندارد</p>
+        <div className={styles.routeToOtherPages}>
+          <Link to="/" className={`${styles.btn} ${styles.btn_default} ${styles.btn_lg} ${styles.btn3d}`}>
+            صفحه اصلی
+          </Link>
+          <Link to="/signin" className={`${styles.btn} ${styles.btn_default} ${styles.btn_lg} ${styles.btn3d}`}>
+            ورود به حساب
+          </Link>
+          <a className={`${styles.btn} ${styles.btn_default} ${styles.btn_lg} ${styles.btn3d}`} href="http://92.63.169.226:8080/">
+            توسعه دهندگان
+          </a>
+        </div>
       </div>
     </div>
   );
