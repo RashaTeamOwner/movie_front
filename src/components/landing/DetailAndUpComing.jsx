7f4d598779feee86cb4569678411d3c2c178135c
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2024-01-16 11:55:04 -1100

    remove parallax

--- src/components/landing/DetailAndUpComing.jsx
+++ src/components/landing/DetailAndUpComing.jsx
@@ -1,23 +1,10 @@
 /* eslint-disable react/no-unknown-property */
 import styles from "./DetailAndUpComing.module.scss";
-import img1 from "../../assets/loginpage/backdrop.jpg";
-import { Parallax } from "react-scroll-parallax";
 function DetailAndUpComing() {
-  return (
-    <div>
-      <Parallax opacity={[0, 1]} scale={[0.5, 1]} speed={10} style={{ position: "fixed", top: "290px" }}>
-        <div className={styles.mainConatainer}>
-          <img src={img1} alt="" />
-        </div>
-      </Parallax>
-      <Parallax opacity={[0, 1]} translateX={["-1200px", "100px"]} speed={5} style={{ position: "fixed", top: "340px" }}>
-        <div className={styles.mainConatainer}>
-          <img src={img1} alt="" />
-        </div>
-      </Parallax>
-      <div style={{ height: "12000px" }}></div>
-    </div>
-  );
+  const handleScroll = (ele) => {
+    console.log(ele);
+  };
+  return <div className={styles.mainConatainer} onScrollCapture={handleScroll}></div>;
 }
 
 export default DetailAndUpComing;
