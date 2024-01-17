8db41d7997792ac6bb7d62bf3580b809bae91376
hosinrg1380@gmail.com <hosinrg1380@gmail.com>
2024-01-17 11:19:52 -1100

    fix layout myMovies in destop vision

--- src/components/landing/SearchMovie/rightbox/MyMovies.jsx
+++ src/components/landing/SearchMovie/rightbox/MyMovies.jsx
@@ -2,6 +2,8 @@
 import styles from "./Mymovies.module.scss";
 import arrow from "../../../../assets/loginpage/arrow.svg";
 import img1 from "../../../../assets/landing/upcoming/7s.webp";
+import img2 from "../../../../assets/landing/upcoming/11s.webp";
+import img3 from "../../../../assets/landing/upcoming/16s.webp";
 function MyMovies() {
   return (
     <div className={styles.mainContainer}>
@@ -27,10 +29,57 @@ function MyMovies() {
               <p>
                 ژانر فیلم : <span>درام , اجتماعی , ترسناک</span>
               </p>
-              <button>اطلاعات فیلم</button>
             </div>
+            <button>اطلاعات فیلم</button>
           </div>
-          <button>حذف فیلم</button>
+          <div className={styles.optionSetup}>
+            <button>حذف فیلم</button>
+            <p>پخش آنلاین به زودی ...</p>
+          </div>
+        </div>
+        <div className={styles.listMovie}>
+          <img src={img2} alt="movie" />
+          <div className={styles.detailMovie}>
+            <h2>From</h2>
+            <div className={styles.votes}>
+              <p>
+                نمره شما : <span>7</span>
+              </p>
+              <p>
+                نمره فیلم : <span>8.6</span>
+              </p>
+              <p>
+                ژانر فیلم : <span>درام , اجتماعی , ترسناک</span>
+              </p>
+            </div>
+            <button>اطلاعات فیلم</button>
+          </div>
+          <div className={styles.optionSetup}>
+            <button>حذف فیلم</button>
+            <p>پخش آنلاین به زودی ...</p>
+          </div>
+        </div>
+        <div className={styles.listMovie}>
+          <img src={img3} alt="movie" />
+          <div className={styles.detailMovie}>
+            <h2>From</h2>
+            <div className={styles.votes}>
+              <p>
+                نمره شما : <span>7</span>
+              </p>
+              <p>
+                نمره فیلم : <span>8.6</span>
+              </p>
+              <p>
+                ژانر فیلم : <span>درام , اجتماعی , ترسناک</span>
+              </p>
+            </div>
+            <button>اطلاعات فیلم</button>
+          </div>
+          <div className={styles.optionSetup}>
+            <button>حذف فیلم</button>
+            <p>پخش آنلاین به زودی ...</p>
+          </div>
         </div>
       </div>
     </div>
